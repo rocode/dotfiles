68904bfe1b7973e508f3181c18bb54d5e751b714
Elias Roa <elias@roa.email>
2024-06-17 10:48:38 -0600

    chore(avim): Update community plugin list

--- private_dot_config/avim/lua/community.lua
+++ private_dot_config/avim/lua/community.lua
@@ -12,6 +12,8 @@ return {
 	{ import = "astrocommunity.colorscheme.dracula-nvim" },
 	{ import = "astrocommunity.diagnostics.trouble-nvim" },
 	{ import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+	{ import = "astrocommunity.lsp.actions-preview-nvim" },
+	{ import = "astrocommunity.note-taking.zk-nvim" },
 	{ import = "astrocommunity.pack.bash" },
 	{ import = "astrocommunity.pack.cpp" },
 	{ import = "astrocommunity.pack.docker" },
